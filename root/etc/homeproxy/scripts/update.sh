0e960bfc9210d8aa8b31b6d457434503246301cd
yupeng10 <yupeng10@staff.weibo.com>
2025-05-29 21:17:37 +0800

    my_self

--- root/etc/homeproxy/scripts/update.sh
+++ root/etc/homeproxy/scripts/update.sh
@@ -5,9 +5,13 @@ CONFIG_URL2="http://192.168.5.2:50000/config.json?template=wrt_template.json"
 
 TEMP_CONFIG="/tmp/sing-box-config_2.json"
 TARGET_CONFIG="/etc/homeproxy/scripts/sing-box-normal.json"
-
-
+LOG_FILE="/var/run/homeproxy/update.log"
+RUN_CONFIG="/var/run/homeproxy/sing-box-c.json"
+log_time() {
+    date +"%Y-%m-%d %H:%M:%S"
+}
 mkdir -p /var/run/homeproxy
+echo "$(log_time) - start" >> "$LOG_FILE"
 rm -f "$TEMP_CONFIG"
 
 /usr/bin/curl -fsSL  --connect-timeout 5 --max-time 10 "$CONFIG_URL" -o "$TEMP_CONFIG"
@@ -15,18 +19,33 @@ if [ ! -s "$TEMP_CONFIG" ]; then
   /usr/bin/curl -fsSL  --connect-timeout 5 --max-time 10 "$CONFIG_URL2" -o "$TEMP_CONFIG"
 fi
 
+# Check if the temporary configuration file exists
 if [ ! -s "$TEMP_CONFIG" ]; then
-  echo "updatesh TEMP_CONFIG not exist"
-  return 1
+    echo "$(log_time) - $TEMP_CONFIG not exist" >> "$LOG_FILE"
+    exit 1
 fi
 
+# Check JSON format
 if ! jsonfilter -i "$TEMP_CONFIG" -e '@' >/dev/null 2>&1; then
-  echo "Invalid JSON format, update failed."
-  return 1
+    echo "$(log_time) - Invalid JSON format, update failed." >> "$LOG_FILE"
+    exit 1
 fi
-
+# Update the configuration file
 cp -f "$TEMP_CONFIG" "$TARGET_CONFIG"
-/etc/init.d/homeproxy restart
+
+
+# Log the successful update
+echo "$(log_time) - update config succ" >> "$LOG_FILE"
+#
+if [ -f "$RUN_CONFIG" ]; then
+    if ! cmp -s "$RUN_CONFIG" "$TARGET_CONFIG"; then
+        echo "$(log_time) - config changed homeproxy restart" >> "$LOG_FILE"
+        /etc/init.d/homeproxy restart
+    else
+        echo "$(log_time) - config no change " >> "$LOG_FILE"
+    fi
+fi
+exit 0
 
 
 
